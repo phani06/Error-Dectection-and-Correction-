3784dfa646cb19f0f1899157bd4654069bb1fddc
Phanindhar T <67418559+phani06@users.noreply.github.com>
2022-02-27 23:57:21 +0530

    Update hamming.java

--- hamming.java
+++ hamming.java
@@ -9,7 +9,7 @@ class HammingCode {
         System.out.println();
     }
   
-    // Calculating Value of Redundant Bits
+    // Calculating value of Redundant Bits
     static int[] calculation(int[] ar, int r)
     {
         for (int i = 0; i < r; i++) {
