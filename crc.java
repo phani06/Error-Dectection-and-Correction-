dc80c1e7509d3325458fbadd4ef39544b63aa2eb
Phanindhar T <67418559+phani06@users.noreply.github.com>
2022-03-02 22:28:00 +0530

    Update crc.java

--- crc.java
+++ crc.java
@@ -1,7 +1,7 @@
 // Import required classes and packages  
 package javaTpoint.MicrosoftJava;  
 import java.util.*;  
-// create crCexample class to demonstrate the working of Cyclic Redundancy Check  
+// create crcexample class to demonstrate the working of Cyclic Redundancy Check  
 class CRCExample {  
     // main() method start  
     public static void main(String args[]) {  
